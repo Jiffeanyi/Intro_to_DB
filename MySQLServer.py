605d08c43b1d8d4a0e2d9a9f1ceb3ea8b1ba8451
Nwosu Joseph Ifeanyi <jokechi2991@gmail.com>
2025-08-17 22:18:25 +0100

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,7 +4,6 @@ A simple Python script to create the alx_book_store database.
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
@@ -20,7 +19,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
@@ -30,3 +29,4 @@ def create_database():
 
 if __name__ == "__main__":
     create_database()
+
